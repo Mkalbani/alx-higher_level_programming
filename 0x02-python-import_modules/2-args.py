25cd8d69a23f16d959b91a027703974129e167b2
mkothm <mkothman.jr@gmail.com>
2022-12-01 13:53:57 -0800

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     i = len(argv)
     if i == 1:
-        print("0 arguments.")
+        print("{} arguments.".format(i - 1))
     elif i >= 1:
         print("{} arguments:".format(i - 1))
     for j in range(1, i):
