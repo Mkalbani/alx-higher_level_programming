1d5caf9ec5e434d410fa5fa6f80b8ed9957ba3c3
mkothm <mkothman.jr@gmail.com>
2022-12-01 13:51:45 -0800

    new

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    i = len(argv)
+    if i == 1:
+        print("0 arguments.")
+    elif i >= 1:
+        print("{} arguments:".format(i - 1))
+    for j in range(1, i):
+        print("{}: {}".format(j, argv[j]))
+
