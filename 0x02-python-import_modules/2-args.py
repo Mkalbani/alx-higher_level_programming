8860ca573c8ae65f803832faeb0fa0fd96b5fe50
mkothm <mkothman.jr@gmail.com>
2022-12-01 14:01:03 -0800

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     i = len(argv)
     if i == 1:
         print("{} arguments.".format(i - 1))
-    elif i >= 1:
-        print("{} arguments:".format(i - 1))
+    elif i == 2:
+        print("{} argument:".format(i - 1))
+    elif i > 2:
+        print("{} arguments:".format(i -1))
     for j in range(1, i):
         print("{}: {}".format(j, argv[j]))
