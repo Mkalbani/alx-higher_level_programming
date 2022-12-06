4c0ff3444ced6a0d19caa2e07c8b4acce68d8532
mkothm <mkothman.jr@gmail.com>
2022-12-06 12:49:39 -0800

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif i == 2:
         print("{} argument:".format(i - 1))
     elif i > 2:
-        print("{} arguments:".format(i -1))
+        print("{} arguments:".format(i - 1))
     for j in range(1, i):
         print("{}: {}".format(j, argv[j]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    n = len(sys.argv)
     sum = 0
-    for i in range(1, n):
-        sum += int(sys.argv[i])
-    print(sum)
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+    print("{}".format(sum))
