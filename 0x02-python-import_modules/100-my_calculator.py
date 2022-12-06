6d1bc050daf44dc2f28ea5bfee95d52aada27a79
mkothm <mkothman.jr@gmail.com>
2022-12-06 13:45:20 -0800

    new

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     argc = len(argv) - 1
     if argc < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
     if argv[2] == "+":
