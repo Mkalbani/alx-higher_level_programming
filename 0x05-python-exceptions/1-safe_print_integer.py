1d6e6a42f8b62d47ec4e68e0a2cba36eb3caf0b5
mkothm <mkothman.jr@gmail.com>
2022-12-19 11:39:41 -0800

    new

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-     try:
-         if value == int:
-             print("{:d}".format(value))
-     except ValueError:
+    try:
+        #if value == int:
+        print("{:d}".format(value))
+    except ValueError:
         return False
+    else:
+        return True
