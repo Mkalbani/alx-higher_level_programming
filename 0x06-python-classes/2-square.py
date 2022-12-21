36548bc164b36e61612f63613797c534579842cb
mkothm <mkothman.jr@gmail.com>
2022-12-21 14:45:53 -0800

    new

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,10 +9,13 @@ class Square:
     def __init__(self, size=0):
         """initializes an optional
         size
+
+        Args:
+            __size (int): The __size 
+            the new square.
         """
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0"
-        else:
-        self.__size = size
